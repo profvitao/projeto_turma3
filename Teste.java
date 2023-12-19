0d431ec9d633bf64bcaf8b327daaca42ac1d2dce
Celso Furtado <celso.furtado@sp.senai.br>
2023-12-19 10:32:24 -0300

    Teste java

---
+++ Teste.java
@@ -0,0 +1,12 @@
+public class Teste {
+
+    public static void main(String[] args) {
+
+        System.out.println("Olá mundo");
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
+
+    }
+
+}
